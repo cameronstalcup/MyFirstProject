20bce58eed056e92e9d4f500e96ab07f4a373688
cameronstalcup <41632985+cameronstalcup@users.noreply.github.com>
2018-07-24 12:30:54 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyFirstC_Program
+{
+    class Program
+    {
+        public enum CustomerStatus
+        {
+            New = 1,
+            AwaitingActivation = 2,
+            Activated = 3,
+            DeActivated = 4
+        }
+        public struct Customer
+        {
+            public int Id;
+            public string FirstName;
+            public string LastName;
+            public CustomerStatus Status;
+        }
+        static void Main(string[] args)
+        {
+            Customer cust = new Customer();
+            cust.Id = 101;
+            cust.FirstName = "Ajay";
+            cust.LastName = "Singala";
+            //cust.Status = (CustomerStatus) 1;
+            cust.Status = CustomerStatus.New;
+            System.Console.WriteLine($"{cust.Id}-{cust.FirstName}-{cust.LastName}-{cust.Status}");
+        }
+    }
+}
